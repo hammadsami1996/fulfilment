9df9b23710490bb0f6531f393017c1da51ba82a4
hammadsami1996 <88374143+hammadsami1996@users.noreply.github.com>
2023-08-02 18:33:47 +0500

    ok

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,13 @@ import {createStore} from 'vuex';
 import App from './App.vue';
 import routes from './routes/routes';
 import storeData from './store/store';
-import filters from './libs/filters';
+import filters from './libs/filters.js';
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 import ToastPlugin from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-default.css';
 import 'tailwindcss/tailwind.css';
+import PrimeVue from 'primevue/config';
 
 window.Alpine = Alpine;
 Alpine.plugin(focus);
@@ -22,5 +23,6 @@ app.use(store);
 app.use(filters);
 app.use(routes);
 app.use(ToastPlugin);
+app.use(PrimeVue);
 
 app.mount('#app');
