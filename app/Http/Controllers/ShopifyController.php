2594bf12335d658adde9074fff04017ee8f265d5
Nazar <nazarnasim101@gmail.com>
2023-09-18 15:02:31 +0500

    woocommerce store orders

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Models\Store;
 use App\Models\Order;
-
+use App\Models\Customer;
+use App\Models\Product;
 class ShopifyController extends Controller
 {
     public function fetchData(Request $request)
@@ -21,9 +23,9 @@ class ShopifyController extends Controller
             // Check if the request was successful
             if ($response->successful()) {
                 $data = $response->json(); // No need to decode JSON manually
-                $this->storeOrder(1);
+                // $this->storeOrder(1);
                 // return response()->json($data);
-            // return response()->json(['data' => true]);
+            return response()->json(['data' => true]);
 
             } else {
                 // Handle the error, e.g., log it or return an error response
@@ -66,10 +68,45 @@ class ShopifyController extends Controller
                     // dd($rec['customer']['default_address']['address1']);
                     $order = Order::where('external_order_no', $rec['id'])->where('order_form','shopify');
                     if (!$order->first()) {
-                        // dd('abcd');
+                        // dd('abcd');  
                         $order = new Order();
                         $order->order_form = 'shopify';
                         $order->external_order_no = $rec['id'];
+                        // dd('abcd');
+                        if ($rec['customer']['phone']) {
+                            $customer = Customer::where('phone', $rec['customer']['phone'])->first();
+                            if ($customer) {
+                                $order->customer_id = $customer['id'];
+                            } else {
+                                $customer = new Customer();
+                                $customer->name = $rec['customer']['first_name'];
+                                $customer->s_address_1 =  $rec['customer']['default_address']['address1'] . $rec['customer']['default_address']['address2'];
+                                $customer->b_address_1 =  $rec['customer']['default_address']['address1'] . $rec['customer']['default_address']['address2'];
+                                $customer->email = $rec['customer']['email'];
+                                $customer->phone = $rec['customer']['phone'];
+                                $b_city = City::where('title' , $rec['billing_address']['city'])->first();
+                                $customer->b_city_id = $b_city['id'];
+                                $customer->b_country_id = $b_city['country_id'];
+                                $customer->b_name = $rec['billing_address']['first_name'];
+                                $customer->b_phone = $rec['billing_address']['phone'];
+                                $customer->b_address_1 = $rec['billing_address']['address1'];
+                                $customer->b_address_2 = $rec['billing_address']['address2'];
+
+
+                                $s_city = City::where('title' , $rec['shipping_address']['city'])->first();
+                                $customer->s_city_id = $s_city['id'];
+                                $customer->s_country_id = $s_city['country_id'];
+                                $customer->s_name = $rec['shipping_address']['first_name'];
+                                $customer->s_phone = $rec['shipping_address']['phone'];
+                                $customer->s_address_1 = $rec['shipping_address']['address1'];
+                                $customer->s_address_2 = $rec['shipping_address']['address2'];
+
+
+                                $customer->save();
+                                // dd($customer['id']);        
+                                $order->customer_id = $customer['id'];
+                            }
+                        }
                         $order->name = $rec['customer']['first_name'];
                         $order->email = $rec['customer']['email'];
                         $order->phone = $rec['customer']['phone'];
@@ -98,12 +135,21 @@ class ShopifyController extends Controller
                         // dd($rec['line_items ']);
                         $items = [];
                         foreach ($rec['line_items'] as $key => $item) {
+                            $product = Product::where('product_sku',$item['sku'])->first();
+                                if (!$product){
+                                    $product = new Product();
+                                    $product->product_sku = $item['sku'];
+                                    $product->title = $item['title'];
+                                    $product->selling_price = $item['price'];
+                                    $product->cost_price = $item['price'];
+                                    $product->save();
+                                }
                             // dd($item);
                             $items[$key]['qty'] = $item['quantity'];
                             // $items[$key]['value_inc_tax'] = $item['total'];
                             // $items[$key]['value_ex_tax'] = $item['subtotal'];
 
-                            // $items[$key]['product_id'] = $item['product_id'];
+                            $items[$key]['product_id'] = $product['id'];
                             $items[$key]['unit_price'] = $item['price'];
                             // $items[$key]['tax_amount'] = $item['total_tax'];
 
