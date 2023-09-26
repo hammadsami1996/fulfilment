f1357658a8193e4271639e546dca5a410b615a4d
hamza-mimsoft <hamza@mimsoft.pk>
2023-09-26 11:37:42 +0500

    Hamza

--- resources/js/views/company_settings/global_settings.vue
+++ resources/js/views/company_settings/global_settings.vue
@@ -29,15 +29,6 @@
                 </div>
                 <!-- <span class="text-gray-500 font-bold text-sm">Add Company</span> -->
             </div>
-            <div class="w-full sm:w-1/12 pl-3 sm:mb-0 shows pb-4">
-                <div @click="addStore" class="card">
-                    <i class="fa-regular fa-square-plus fa-2x cursor-pointer"></i>
-                    <div class="card__content">
-                        <p class="card__title">Add Store</p>
-                    </div>
-                </div>
-                <!-- <span class="text-gray-500 font-bold text-sm">Add Store</span> -->
-            </div>
         </div>
 
         <hr/>
@@ -89,7 +80,7 @@
             </label>
             <label>
                 <input
-                    @click="couriertabs"
+                    @click="storetabs"
                     class="radio-input"
                     name="engine"
                     type="radio"
@@ -100,6 +91,20 @@
                           d="M547.6 103.8L490.3 13.1C485.2 5 476.1 0 466.4 0H109.6C99.9 0 90.8 5 85.7 13.1L28.3 103.8c-29.6 46.8-3.4 111.9 51.9 119.4c4 .5 8.1 .8 12.1 .8c26.1 0 49.3-11.4 65.2-29c15.9 17.6 39.1 29 65.2 29c26.1 0 49.3-11.4 65.2-29c15.9 17.6 39.1 29 65.2 29c26.2 0 49.3-11.4 65.2-29c16 17.6 39.1 29 65.2 29c4.1 0 8.1-.3 12.1-.8c55.5-7.4 81.8-72.5 52.1-119.4zM499.7 254.9l-.1 0c-5.3 .7-10.7 1.1-16.2 1.1c-12.4 0-24.3-1.9-35.4-5.3V384H128V250.6c-11.2 3.5-23.2 5.4-35.6 5.4c-5.5 0-11-.4-16.3-1.1l-.1 0c-4.1-.6-8.1-1.3-12-2.3V384v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V384 252.6c-4 1-8 1.8-12.3 2.3z"/></svg>
                     </span>
                     <span class="radio-label">Stores</span>
+                </span>
+            </label>
+            <label>
+                <input
+                    @click="couriertabs"
+                    class="radio-input"
+                    name="engine"
+                    type="radio"
+                />
+                <span class="radio-tile">
+                    <span class="radio-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+                    </span>
+                    <span class="radio-label">Courier</span>
                 </span>
             </label>
         </div>
@@ -297,11 +302,20 @@
                                         <img src="~@/images/WooCommerce.png" class="w-16 h-16 rounded ml-3" v-if="stores.plate_form == 'WooCommerce'" @click="ecommerce('WooCommerce')"/>
                                         <img src="~@/images/MimCart.jpg" class="w-16 h-16 rounded ml-3" v-if="stores.plate_form == 'MimCart'" @click="ecommerce('MimCart')"/>
                                     </span>
-                                    <span class="radio-label font-bold">{{stores ?  stores.name:'' }} Store</span>
+                                    <span class="radio-label font-bold">{{stores ?  stores.name:'' }}</span>
                                 </span>
                             </label>
                         </div>
                     </div>
+                </div>
+                <div class="w-full sm:w-1/12 pl-3 sm:mb-0 shows pb-4">
+                    <div @click="addStore" class="card">
+                        <i class="fa-regular fa-square-plus fa-2x cursor-pointer"></i>
+                        <div class="card__content">
+                            <p class="card__title">Add Store</p>
+                        </div>
+                    </div>
+                    <!-- <span class="text-gray-500 font-bold text-sm">Add Store</span> -->
                 </div>
             </div>
         </div>
@@ -953,7 +967,7 @@
                     })
                 }
             },
-            couriertabs() {
+            storetabs() {
                 (this.email = false), (this.sms = false), (this.courier = true);
                 console.log(this.company_id)
                 if (isNaN(this.company_id)) {
@@ -1042,6 +1056,12 @@
             savestore(){
                 byMethod(this.method, '/api/stores', this.form).then(res => {
                     this.successfull(res)
+                    if(res.data.saved == true){
+                        byMethod("GET", '/api/stores').then(res => {
+                            console.log(res.data.data.data)
+                            this.stores_data = res.data.data.data
+                        })
+                    }
                     this.$toast.open({
                         position: 'top-right',
                         message: this.mode === 'edit' ? 'Update Successfully' : 'Create Successfully',
