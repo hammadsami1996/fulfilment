a5cb59000190afde02bc7fae66e2921c8839ec13
iqrahafeez98 <iqra.hafeez@mimsoft.pk>
2023-09-28 18:54:03 +0500

    28/sep

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -112,7 +112,7 @@ class ProductController extends Controller
      */
     public function edit($id)
     {
-        $model = Product::with('product_img', 'brand', 'category')->findOrFail($id);
+        $model = Product::with('product_img', 'brand', 'category', 'supplier')->findOrFail($id);
 //        $model->Settings()->get('company_name');
 
         return response()->json([
