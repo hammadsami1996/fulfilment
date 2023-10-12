82879696225bcd90a0cd054b19c3ed7518fca56d
hammadsami1996 <88374143+hammadsami1996@users.noreply.github.com>
2023-10-12 16:06:07 +0500

    ok

--- app/Models/ProductAttribute.php
+++ app/Models/ProductAttribute.php
@@ -2,11 +2,10 @@
 
 namespace App\Models;
 
-use App\Support\Search;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ProductAttributeGroup extends Model
+class ProductAttribute extends Model
 {
-    use HasFactory, Search;
+    use HasFactory;
 }
