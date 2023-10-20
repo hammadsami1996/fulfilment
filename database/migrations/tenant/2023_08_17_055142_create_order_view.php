6a899bed4b7ae9c88f388a9eb2d491bed468fa03
hammadsami1996 <88374143+hammadsami1996@users.noreply.github.com>
2023-10-20 19:08:07 +0500

    Update 2023_08_17_055142_create_order_view.php

--- database/migrations/tenant/2023_08_17_055142_create_order_view.php
+++ database/migrations/tenant/2023_08_17_055142_create_order_view.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -10,8 +9,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        $sql = "select `o`.*,(case when exists(select 1 from ((`order_items` `oi` left join `products` `p` on((`p`.`id` = `oi`.`product_id`))) left join `inventories` `i` on(((`oi`.`product_id` = `i`.`product_id`) and (`o`.`warehouse_id` = `i`.`warehouse_id`)))) where ((`oi`.`order_id` = `o`.`id`) and (`oi`.`qty` > coalesce(`i`.`qty`,0)) and (`p`.`manage_inventory` = 1))) then 'unpackable' else 'packable' end) AS `packability` from `orders` `o` where (`o`.`deleted_at` is null)";
+//        $sql = "select `o`.*,(case when exists(select 1 from ((`order_items` `oi` left join `products` `p` on((`p`.`id` = `oi`.`product_id`))) left join `inventories` `i` on(((`oi`.`product_id` = `i`.`product_id`) and (`o`.`warehouse_id` = `i`.`warehouse_id`)))) where ((`oi`.`order_id` = `o`.`id`) and (`oi`.`qty` > coalesce(`i`.`qty`,0)) and (`p`.`manage_inventory` = 1))) then 'unpackable' else 'packable' end) AS `packability` from `orders` `o` where (`o`.`deleted_at` is null)";
 
+        $sql = "CREATE VIEW order_views AS
+       select `o`.*,(case when exists(select 1 from ((`order_items` `oi` left join `products` `p` on((`p`.`id` = `oi`.`product_id`)))
+        left join `inventories` `i` on(((`oi`.`product_id` = `i`.`product_id`) and (`o`.`warehouse_id` = `i`.`warehouse_id`))))
+        where ((`oi`.`order_id` = `o`.`id`) and (`oi`.`qty` > coalesce(`i`.`qty`,0)) and (`p`.`manage_inventory` = 1)))
+        then 'unpackable' else 'packable' end) AS `packability` from `orders` `o` where (`o`.`deleted_at` is null)";
         DB::statement($sql);
     }
     /**
