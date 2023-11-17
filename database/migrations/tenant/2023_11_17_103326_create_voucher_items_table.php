74486eb91f4878b7aacc3e6a52b477ea612ac9f1
iqrahafeez98 <iqra.hafeez@mimsoft.pk>
2023-11-17 15:41:04 +0500

    migrations

---
+++ database/migrations/tenant/2023_11_17_103326_create_voucher_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('voucher_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('account_id', 15)->nullable();
+            $table->string('subledger', 15)->nullable();
+            $table->string('debit', 15)->nullable();
+            $table->string('credit', 15)->nullable();
+            $table->string('reference', 15)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('voucher_items');
+    }
+};
