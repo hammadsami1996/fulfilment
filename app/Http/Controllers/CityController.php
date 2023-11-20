a6ac98e60f224792932fca4b232793374bbdc19c
hammadsami1996 <88374143+hammadsami1996@users.noreply.github.com>
2023-11-20 16:32:04 +0500

    Update CityController.php

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -138,12 +138,12 @@ class CityController extends Controller
         }
 
         $excludeCondition = $request->has('exclude') && $request->exclude == 1 && ($request->city_id || $request->country_id);
-        $includeCondition = (!$request->has('exclude') || $request->exclude == 0) && ($request->city_id || $request->country_id);
+        $includeCondition = (!$request->has('exclude') || $request->exclude == 0) && $request->city_id && $request->country_id;
         if ($excludeCondition) {
             $cityIds = City::where('country_id', $request->country_id)->whereNotIn('id',[$request->city_id])->pluck('id')->toArray();
         } elseif ($includeCondition) {
             $cityIds = City::where('country_id', $request->country_id)->whereIn('id',[$request->city_id])->pluck('id')->toArray();
-        } elseif ($request->city_id) {
+        } elseif ($request->city_id && !$request->country_id) {
             $cityIds = [$request->city_id];
         } else {
             $cityIds = City::where('country_id', $request->country_id)->pluck('id')->toArray();
