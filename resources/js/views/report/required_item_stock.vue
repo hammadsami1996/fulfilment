c65652e07c745e8bb8f2de14d1678befd271e85c
ubaid <ubaidahmed1282@gmail.com>
2023-11-23 17:47:21 +0500

    ubaid

--- resources/js/views/report/required_item_stock.vue
+++ resources/js/views/report/required_item_stock.vue
@@ -16,11 +16,11 @@
         <div class="flex-row">
 
             <div class="flex flex-row">
-            <div class="flex flex-col-6 ml-6 mt-4" colspan="8">
+            <div class=" flex-col-6 ml-6 mt-4" colspan="8">
                 <button
 
                 @click="downloadImage"
-                class="inline-flex justify-center items-center space-x-2 border font-semibold rounded-lg px-3 py-2 leading-5 text-sm border-gray-200 bg-blue-400 text-white"
+                class="inline-flex justify-center items-center space-x-2 border font-semibold rounded-lg px-3 py-3 leading-5 text-sm border-gray-200 bg-blue-400 text-white"
                 type="button">
                 <svg style="margin-right: 10px;" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z" fill="white"/></svg>
                 Download Images
