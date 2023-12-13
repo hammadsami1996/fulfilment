2b7a1606a07ac20393c2c0cac3742f3e8faa982e
ubaid <ubaidahmed1282@gmail.com>
2023-12-13 11:35:53 +0500

    ubaid

--- database/migrations/tenant/2023_05_25_074915_create_account_group_classes_table.php
+++ database/migrations/tenant/2023_05_25_074915_create_account_group_classes_table.php
@@ -25,96 +25,96 @@ return new class extends Migration
             $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
         });
-        // DB::unprepared(file_get_contents(public_path('/account_group_classes.sql')));
+        DB::unprepared(file_get_contents(public_path('/account_group_classes.sql')));
 
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => 101,
-                'classname' => 'Current Asset', 
-                'class_type_id' => 1, 
-                'active' => 1
-            ]
-        );
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => 102,
-                'classname' => 'Fixed Asset', 
-                'class_type_id' => 1, 
-                'active' => 1
-            ]
-        );
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => 201,
-                'classname' => 'Current Liabilities', 
-                'class_type_id' => 2, 
-                'active' => 1
-            ]
-        );
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => 202,
-                'classname' => 'Long Term Liabilties', 
-                'class_type_id' => 2, 
-                'active' => 1
-            ]
-        );
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => 401,
-                'classname' => 'Income', 
-                'class_type_id' => 4, 
-                'active' => 1
-            ]
-        );
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => 501,
-                'classname' => 'Expenses', 
-                'class_type_id' => 5, 
-                'active' => 1
-            ]
-        );
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => 301,
-                'classname' => 'Equity', 
-                'class_type_id' => 3, 
-                'active' => 1
-            ]
-        );
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => null,
-                'classname' => 'Cost of Goods Sold', 
-                'class_type_id' => 5, 
-                'active' => 1
-            ]
-        );
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => null,
-                'classname' => 'dRAWING', 
-                'class_type_id' => 3, 
-                'active' => 1
-            ]
-        );
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => 302,
-                'classname' => 'DRAWING', 
-                'class_type_id' => 3, 
-                'active' => 1
-            ]
-        );
-        DB::table('account_group_classes')->insert(
-            [
-                'code' => 103,
-                'classname' => 'Long Term Assets', 
-                'class_type_id' => 1, 
-                'active' => 1
-            ]
-        );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => 101,
+        //         'classname' => 'Current Asset', 
+        //         'class_type_id' => 1, 
+        //         'active' => 1
+        //     ]
+        // );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => 102,
+        //         'classname' => 'Fixed Asset', 
+        //         'class_type_id' => 1, 
+        //         'active' => 1
+        //     ]
+        // );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => 201,
+        //         'classname' => 'Current Liabilities', 
+        //         'class_type_id' => 2, 
+        //         'active' => 1
+        //     ]
+        // );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => 202,
+        //         'classname' => 'Long Term Liabilties', 
+        //         'class_type_id' => 2, 
+        //         'active' => 1
+        //     ]
+        // );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => 401,
+        //         'classname' => 'Income', 
+        //         'class_type_id' => 4, 
+        //         'active' => 1
+        //     ]
+        // );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => 501,
+        //         'classname' => 'Expenses', 
+        //         'class_type_id' => 5, 
+        //         'active' => 1
+        //     ]
+        // );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => 301,
+        //         'classname' => 'Equity', 
+        //         'class_type_id' => 3, 
+        //         'active' => 1
+        //     ]
+        // );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => null,
+        //         'classname' => 'Cost of Goods Sold', 
+        //         'class_type_id' => 5, 
+        //         'active' => 1
+        //     ]
+        // );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => null,
+        //         'classname' => 'dRAWING', 
+        //         'class_type_id' => 3, 
+        //         'active' => 1
+        //     ]
+        // );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => 302,
+        //         'classname' => 'DRAWING', 
+        //         'class_type_id' => 3, 
+        //         'active' => 1
+        //     ]
+        // );
+        // DB::table('account_group_classes')->insert(
+        //     [
+        //         'code' => 103,
+        //         'classname' => 'Long Term Assets', 
+        //         'class_type_id' => 1, 
+        //         'active' => 1
+        //     ]
+        // );
     }
 
     /**
